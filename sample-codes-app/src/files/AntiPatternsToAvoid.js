66bf2587714b6db395a6aa1f60f87a8818deb5d3
Nidheesh PV <nidheesh@vonnue.com>
2024-02-05 22:25:55 +0530

    feat: Too much work on every state changes

--- sample-codes-app/src/files/AntiPatternsToAvoid.js
+++ sample-codes-app/src/files/AntiPatternsToAvoid.js
@@ -1,13 +1,18 @@
 import Article from "./Article";
 import NavBar from "./Navbar";
 import Parent from "./Parent";
+import WorkHard from "./WorkHard";
 
 
 const AntiPatternsToAvoid = () => {
     return (<div>
+        {/* Big components */}
         <NavBar />
         <Article />
+        {/* Nesting components */}
         <Parent />
+        {/* Too much work */}
+        <WorkHard />
     </div>)
 }
 
@@ -16,3 +21,4 @@ export default AntiPatternsToAvoid;
 // 1. Avoid big components. Break big components into smaller reusable components
 // 2. Avoid nesting components. Nesting components in same files increase memory consumption.
 //    Every rerendering of parent components create new child component.
+// 3. Too much work. The component run on all state changes
