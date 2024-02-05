e0ff8975d4d6ee8063a204a2be0a5abb55df73d5
Nidheesh PV <nidheesh@vonnue.com>
2024-02-05 22:35:15 +0530

    feat: remove pointless divs and structure all files and folders

--- sample-codes-app/src/files/AntiPatternsToAvoid.js
+++ sample-codes-app/src/files/AntiPatternsToAvoid.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Article from "./Article";
 import NavBar from "./Navbar";
 import Parent from "./Parent";
@@ -13,6 +15,15 @@ const AntiPatternsToAvoid = () => {
         <Parent />
         {/* Too much work */}
         <WorkHard />
+        {/* Remove unnecessary div's */}
+        {/* <div>
+            <NavBar />
+            <Article />
+        </div> */}
+        <React.Fragment>
+            <NavBar />
+            <Article />
+        </React.Fragment>
     </div>)
 }
 
@@ -21,4 +32,6 @@ export default AntiPatternsToAvoid;
 // 1. Avoid big components. Break big components into smaller reusable components
 // 2. Avoid nesting components. Nesting components in same files increase memory consumption.
 //    Every rerendering of parent components create new child component.
-// 3. Too much work. The component run on all state changes
+// 3. Too much work. The component run on all state changes. use useMemo hook.
+// 4. Remove pointless div's. use <React.Fragment> or <>.
+// 5. Messy files. structure all files with its own folder.
